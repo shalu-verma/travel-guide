c19e67be5da3dc1c9407b85b3c12e9045cc77499
shalu-verma <114733133+shalu-verma@users.noreply.github.com>
2023-03-04 12:55:38 +0530

    Initial Code

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# travel-guide
+# Travel-India
+
+This repository is for the Travel India track of HACKA-DEMIC hackathon and is owned by Team Error 404.
+
+## Introduction
+
+Hi, Welcome to the Official documentation of the Travel India Website and Web app created by Team Error 404. Here, on this website, we have implemented the state wise toursim and covid dashboard to aware the travellers about the risks of being infected. This project will serve as a single place required to make the tour of a person comfortable and Memorable. Using this website a tourist/traveller can plan his/her trip in a better way. This project presents the culture and heritage of India in a unique way.
+
+## Team Members
+1. Atman Das
+2. Snehasish Pati
+3. Yashraj Jain
+4. Arpan Mahato
+
+## Features
+This Project has lot of features to offer. Some of the top features are mentioned below:
+
+##### -> Get the latest Covid-19 details of India and all the States along with covid guidelines.
+##### -> Featured destinations of India along with the rating, reviews, and location.
+##### -> 360° image view, which will help users to view the destinations before visiting.
+##### -> Have a Travel Assistant chatbot, which will help new users to get their required details at a click.
+##### -> 5 days weather forecast of India and its States implemented in the chatbot.
+##### -> Get Best offers on Hotels and Flights Booking or get the best Tourism Packages at a single place.
+##### -> Engaging users with Notifications
+##### -> Get List of Trending things of the specific destination.
+##### -> Responsive design for better experience in different devices.
+##### -> Make this a PWA.
+
+## Technologies Used
+
+1.  MongoDB
+2.  Firebase
+3.  Express
+4.  React.js
+5.  Node.js
+6.  OpenWeather API
+7.  Covid19API
+8.  Bootstrap
+9.  Html
+10. CSS
+11. Javascript
+12. Dialogflow
+13. Flask
+14. Python
+15. Heroku
+16. Onesignal
+
+Our Website is Live at :
+[https://error404-travelindia.web.app/](https://error404-travelindia.web.app/)
+
+You can watch our project presentation at :
+[https://youtu.be/R9ZPa-jFb3Y](https://youtu.be/R9ZPa-jFb3Y)
+
+## Future Implementations & Usage
+
+Thus,We have thought to implement maximum of the feature's over the website, to let the user's get all the details regarding the Destianation at a single place, and won't go to any other third-party site for city details. We have gave our best to implement in this track, but in future, some of the feature's and content that will be added to the website is followed below:
+
+
+##### -> Calculate Distance of user from the selected destinations.
+##### -> Feature posts/news from different social media platforms related to the state and destination.
+##### -> Dark theme feature.
+
+
+
+
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react'
+import Chart from 'chart.js'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import hero_image from '../assets/img/hero_image.jpg'
+import hero_caption from '../assets/img/hero_caption.png'
+import flight from '../assets/img/Bookings/flight.jpg'
+import hotel from '../assets/img/Bookings/hotel.jpg'
+import pack from '../assets/img/Bookings/package.jpg'
+import "../assets/css/home.css"
+// import "../assets/css/covid.css"
+import StateCarousel from './StateCarousel'
+import DestinationCarousel from './DestinationCarousel'
+import FoodCarousel from './FoodCarousel'
+// import Dashboard from './Dashboard'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const Home = () => {
+  useEffect(() => {
+    firebase.firestore().collection('rating').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        localStorage.setItem(`${doc.data().id + '_count'}`, `${doc.data().count}`);
+        localStorage.setItem(`${doc.data().id + '_user'}`, `${doc.data().user}`);
+      });
+    })
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className='hero-image'>
+        <div className="hero-caption-container">
+          <img src={hero_caption} alt="hero-caption" className="hero-caption" />
+        </div>
+      </div>
+      {/* <Dashboard /> */}
+      <main className="main-container ">
+        {/* <section id="video" className='mt-3'>
+          <h2 className="header mb-4 ">INDIA AT A GLANCE </h2>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/77czO8sxABI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </section> */}
+        <section className="top-states my-5">
+          <h2 className="header">EXPLORE STATES</h2>
+          <StateCarousel />
+        </section>
+        <section className="top-states my-5" id='top-destinations'>
+          <h2 className="header">TOP DESTINATIONS</h2>
+          <DestinationCarousel />
+        </section>
+        <section className="top-states my-5">
+          <h2 className="header">EXPLORE FOODS</h2>
+          <FoodCarousel />
+        </section>
+        <section id="shorcuts">
+          <h2 className="header mb-3 explore">MAKE YOUR PLANS</h2>
+          <div className="card-deck">
+            <div className="card">
+              <a href="/book-flights" className="card-link">
+                <img src={flight} className="card-img-top" alt="Flight" />
+                <span>BOOK FLIGHT</span>
+              </a>
+            </div>
+            <div className="card">
+              <a href="/book-hotels" className="card-link">
+                <img src={hotel} className="card-img-top" alt="Hotel" />
+                <span>BOOK HOTELS</span>
+              </a>
+            </div>
+            <div className="card">
+              <a href="/book-package" className="card-link">
+                <img src={pack} className="card-img-top" alt="package" />
+                <span>BOOK PACKAGE</span>
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
